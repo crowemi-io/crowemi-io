8e72d7bb89139635707094d20413075f701cfe98
Andy Crowe <crowemi@hotmail.com>
2024-02-23 16:52:44 -0800

    tweeks to footer; etc. etc.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,33 +6,39 @@ interface Props {
 
 export default function Footer({ LinkItems }: Props) {
     return (
-        <div className="bg-primary p-10  text-white">
-            <div className="container mx-auto pt-10 pb-10">
-                <div className="grid grid-cols-2">
-                    <div >
-                        <div className="text-5xl font-extrabold">Ready to elevate your business?</div>
-                        <div>we're here to help</div>
+        <>
+            <div className="bg-primary p-10  text-white">
+                <div className="container mx-auto pt-10 pb-10">
+                    <div className="grid grid-cols-2">
+                        <div >
+                            <div className="text-5xl font-extrabold">ready to elevate your business?</div>
+                            <div>we're here to help</div>
+                        </div>
+                        <div className="text-right">2</div>
+
                     </div>
-                    <div className="text-right">2</div>
+                </div>
 
+
+            </div>
+            <div  className="bg-primary p-10  text-white border-t border-t-bc">
+                <div className="flex container mx-auto">
+                    <div className="grow">
+                        Copyrights © {new Date().getFullYear()} All Rights Reserved by <span className="bg-secondary pr-1 pl-1 italic">crowemi-io</span>
+                    </div>
+                    <div>
+                        {
+                            LinkItems.map((linkItem, idx) =>
+                                <>
+                                    <a href={linkItem.route} className="hover:underline">{linkItem.name}</a>
+                                    {idx !== (LinkItems.length - 1) ? <span className="pr-2 pl-2">|</span> : null}
+                                </>
+                            )
+                        }
+                    </div>
                 </div>
             </div>
-            <div className="flex container mx-auto pt-10 pb-10 font-thin">
-                <div className="grow">
-                    Copyrights © {new Date().getFullYear()} All Rights Reserved by <span className="bg-secondary pr-1 pl-1 italic">crowemi-io</span>
-                </div>
-                <div>
-                    {
-                        LinkItems.map((linkItem, idx) =>
-                            <>
-                                <a href={linkItem.route} className="hover:underline">{linkItem.name}</a>
-                                {idx !== (LinkItems.length - 1) ? <span className="pr-2 pl-2">/</span> : null}
-                            </>
-                        )
-                    }
-                </div>
-            </div>
 
-        </div>
+        </>
     )
 }
