34e80b52e5c96d3d6ef49ccb62f7eb2f0aaf21b1
Andy Crowe <crowemi@hotmail.com>
2024-03-22 21:01:23 -0700

    update clour run service

--- infra/main.tf
+++ infra/main.tf
@@ -9,35 +9,43 @@ resource "google_service_account" "service_account" {
     description  = "A service account for ${local.service}"
 }
 
-resource "google_cloud_run_v2_service" "crowemi-io" {
+resource "google_cloud_run_service" "crowemi_io" {
     name     = local.service
-    location = "us-west1"
-    ingress  = "INGRESS_TRAFFIC_ALL"
+    location = local.region
 
     template {
-        containers {
-            image = "us-west1-docker.pkg.dev/${local.project}/crowemi-io/${local.service}:${var.docker_image_tag}"
-            ports {
-              container_port = 3000
-            }
+        spec {
+            containers {
+                image = "us-west1-docker.pkg.dev/${local.project}/crowemi-io/${local.service}:${var.docker_image_tag}"
 
-            startup_probe {
-                initial_delay_seconds = 0
-                timeout_seconds       = 1
-                period_seconds        = 3
-                failure_threshold     = 1
-            }
-            liveness_probe {
-                http_get {
-                    path = "/"
+                ports {
+                    container_port = 3000
+                }
+
+                resources {
+                    limits {
+                        cpu    = "1000m"
+                        memory = "512Mi"
+                    }
                 }
             }
 
+            service_account_name = google_service_account.service_account.name
+            max_instances        = 2
         }
-        service_account = google_service_account.service_account.email
     }
+
+    traffic {
+        percent         = 100
+        latest_revision = true
+    }
+
+    autogenerate_revision_name = true
+    delete_revision_on_update  = true
+    allow_unauthenticated      = true
 }
-resource "google_cloud_run_domain_mapping" "crowemi-io" {
+
+resource "google_cloud_run_domain_mapping" "crowemi_io" {
     location = "us-west1"
     name     = "io.crowemi.com"
 
